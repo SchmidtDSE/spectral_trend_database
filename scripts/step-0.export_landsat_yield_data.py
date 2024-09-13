943befad29493c77af354557e8b2362d4c213db1
brookie guzder-williams <bguzder-williams@berkeley.edu>
2024-09-13 13:37:17 -0700

    add spectral_trend_database/gee, scripts/step-0.export_landsat_yield_data

---
+++ scripts/step-0.export_landsat_yield_data.py
@@ -0,0 +1,179 @@
+""" EXPORT LANDSAT/YIELD DATA
+
+authors:
+    - name: Brookie Guzder-Williams
+
+affiliations:
+    - University of California Berkeley,
+      The Eric and Wendy Schmidt Center for Data Science & Environment
+
+steps:
+
+    1. Load Sample Yield data
+    2. Add geohashes
+    3. For each year:
+        - Add Harmonized Landsat Pixel Values
+        - Save results, local and GCS, as line-deliminated JSON files
+
+outputs:
+
+- local:
+- gcs:
+
+runtime: ~ XXX minutes
+
+License:
+    BSD, see LICENSE.md
+"""
+import ee
+ee.Initialize()
+import re
+from pathlib import Path
+import warnings
+import math
+import geohash
+import pandas as pd
+import xarray as xr
+import mproc
+from spectral_trend_database.config import config as c
+from spectral_trend_database import gcp
+from spectral_trend_database import paths
+from spectral_trend_database.gee import landsat
+from spectral_trend_database.gee import utils as ee_utils
+warnings.filterwarnings(
+    action="ignore",
+    message="invalid value encountered in divide",
+)
+
+
+#
+# CONFIG
+#
+DRY_RUN = False  # TODO: CONFIG OR CML ARG
+YEARS = range(2020, 2022 + 1)
+LIMIT = 3
+SCALE = 30
+CROP_TYPE = 'corn'
+# CROP_TYPE = 'soy'
+BUFFER = 0
+SRC_PATH = f'{c.URL_PREFIX}agriculture_monitoring/CDL/landsat/{CROP_TYPE}_biomass_1999-2022.csv'
+# BUFFER = 30
+HARVEST_START_DATE_TMPL = '{}-09-01'
+HARVEST_END_DATE_TMPL = '{}-12-01'
+PRECISIONS = [5, 7, 9]
+MAX_PROCESSES = 6
+MAX_ERR = 1
+
+
+#
+# CONSTANTS
+#
+DATA_ROOT = f'{c.ROOT_DIR}/{c.LOCAL_DATA_DIR}'
+
+DEST_NAME = c.RAW_BASE_NAME
+LANDSAT_RADIUS = math.ceil(landsat.NOMINAL_SCALE / 2)
+if BUFFER != 'auto':
+    LANDSAT_RADIUS = LANDSAT_RADIUS + BUFFER
+    DEST_NAME = f'{DEST_NAME}.b{BUFFER}'
+if LIMIT:
+    DEST_NAME = f'{DEST_NAME}-lim{LIMIT}'
+
+
+#
+# METHODS
+#
+def get_geohash(row: pd.Series, precision: int) -> str:
+    return geohash.encode(
+        latitude=row.lat,
+        longitude=row.lon,
+        precision=precision)
+
+
+#
+# METHODS
+#
+def get_mean_pixel_values(row: pd.Series) -> xr.Dataset:
+    radius = LANDSAT_RADIUS
+    if BUFFER == 'auto':
+        buffer = row['buffer']
+        if np.isnan(buffer):
+            buffer = 0
+        radius = LANDSAT_RADIUS + buffer
+    start_date = HARVEST_START_DATE_TMPL.format(row['year'] - 1)
+    end_date = HARVEST_END_DATE_TMPL.format(row['year'])
+    geom = ee.Geometry.Point([row['lon'], row['lat']])
+    geom = geom.buffer(radius, MAX_ERR)
+    data_filter = ee.Filter.And(
+        ee.Filter.bounds(geom),
+        ee.Filter.date(start_date, end_date))
+    ic = landsat.harmonized_cloud_masked_rescaled_ic(data_filter=data_filter)
+    ds = ee_utils.get_ee_xrr(
+        ic,
+        geom=geom,
+        scale=landsat.NOMINAL_SCALE)
+    ds = ds.mean(dim=('X', 'Y'), skipna=True)
+    return ds
+
+
+def add_harvest_year_bands_ds(row):
+    row = dict(row).copy()
+    try:
+        ds = get_mean_pixel_values(row)
+        row.update({b: ds[b].data for b in landsat.HARMONIZED_BANDS})
+        row['date'] = list(ds.time.dt.strftime('%Y-%m-%d').data)
+        row['error'] = None
+    except Exception as e:
+        row['error'] = str(e)
+    return row
+
+
+#
+# RUN
+#
+print('load data:')
+# 1. Load Sample Yield data
+print('-', SRC_PATH)
+df = pd.read_csv(SRC_PATH)
+print('- shape:', df.shape)
+df = df.drop_duplicates()
+print('- drop duplicates shape:', df.shape)
+
+# 2. Add geohashes
+df['sample_id'] = df.apply(lambda r: get_geohash(r, 11), axis=1)
+for p in PRECISIONS:
+    df[f'geohash_{p}'] = df.sample_id.apply(lambda v: v[:p])
+
+
+# 3. For each year:
+#     - Add Harmonized Landsat Pixel Values
+#     - Save results, local and GCS, as line-deliminated JSON files
+print(f'RUNNING LANDSAT EXPORT FOR {YEARS}')
+for year in YEARS:
+    print(year, '...')
+    data = df[df.year == year].to_dict('records')[:LIMIT]
+    count = len(data)
+    if count:
+        print(f'RUNNING {DEST_NAME}[{count}] FOR YEAR = {year}')
+        rows = mproc.map_with_threadpool(
+            add_harvest_year_bands_ds,
+            data,
+            max_processes=MAX_PROCESSES)
+        lsat_df = pd.DataFrame(rows)
+        name = f'{DEST_NAME}-{year}.json'
+        local_dest = paths.local(
+            # c.DEST_LOCAL_FOLDER,
+            c.RAW_LOCAL_FOLDER,
+            name)
+        gcs_dest = paths.gcs(
+            # c.DEST_GCS_FOLDER,
+            c.RAW_GCS_FOLDER,
+            name)
+        uri = gcp.save_ld_json(
+            lsat_df,
+            local_dest=local_dest,
+            gcs_dest=gcs_dest,
+            dry_run=DRY_RUN)
+        print(f'COMPLETE[{lsat_df.shape}]:', DEST_NAME)
+
+    else:
+        print(f'NO DATA FOR {year}')
---
+++ spectral_trend_database/gee/landsat.py
@@ -0,0 +1,164 @@
+""" METHODS AND CONSTANTS FOR ACCESSING LANDSAT THROUGH Google Earth Engine
+
+authors:
+    - name: Brookie Guzder-Williams
+
+affiliations:
+    - University of California Berkeley,
+      The Eric and Wendy Schmidt Center for Data Science & Environment
+
+band descriptions:
+    meta:
+        min: 1
+        max: 65455
+        scale: 2.75e-05
+        offset: -0.2
+
+    landsat 8:
+        SR_B2    0.452-0.512 μm    (blue)
+        SR_B3    0.533-0.590 μm    (green)
+        SR_B4    0.636-0.673 μm    (red)
+        SR_B5    0.851-0.879 μm    (nir)
+        SR_B6    1.566-1.651 μm    (swir1)
+        SR_B7    2.107-2.294 μm    (swir2)
+
+    landsat 5/7:
+        SR_B1    0.45-0.52 μm    (blue)
+        SR_B2    0.52-0.60 μm    (green)
+        SR_B3    0.63-0.69 μm    (red)
+        SR_B4    0.77-0.90 μm    (near infrared)
+        SR_B5    1.55-1.75 μm    (swir1)
+        SR_B7    2.08-2.35 μm    (swir2)
+
+License:
+    BSD, see LICENSE.md
+"""
+from typing import Union, Optional
+import ee
+
+
+#
+# CONSTANTS
+#
+NOMINAL_SCALE = 30
+GRID_DEGREE_SIZE = 0.0439453125
+LSAT_SCALE_FACTOR = 0.0000275
+LSAT_OFFSET = -0.2
+TRANSFORM = [GRID_DEGREE_SIZE, 0, 0, 0, -GRID_DEGREE_SIZE, 0]
+MASK_VALUE = 2.1474836e9
+QA_BANDS = ['QA_PIXEL', 'QA_RADSAT']
+L8_SR_ID = 'LANDSAT/LC08/C02/T1_L2'
+L7_SR_ID = 'LANDSAT/LE07/C02/T1_L2'
+L5_SR_ID = 'LANDSAT/LT05/C02/T1_L2'
+L8_BANDS = [
+    'SR_B2',
+    'SR_B3',
+    'SR_B4',
+    'SR_B5',
+    'SR_B6',
+    'SR_B7'
+]
+L57_BANDS = [
+    'SR_B1',
+    'SR_B2',
+    'SR_B3',
+    'SR_B4',
+    'SR_B5',
+    'SR_B7'
+]
+HARMONIZED_BANDS = [
+    'blue',
+    'green',
+    'red',
+    'nir',
+    'swir1',
+    'swir2'
+]
+MISSIONS = {
+    8: {
+        'id': L8_SR_ID,
+        'bands': L8_BANDS,
+        "dates": "2013-03-18T15:58:14 -"
+    },
+    7: {
+        'id': L7_SR_ID,
+        'bands': L57_BANDS,
+        "dates": "1999-05-28T01:02:17 -"
+    },
+    5: {
+        'id': L5_SR_ID,
+        'bands': L57_BANDS,
+        'dates': "1984-03-16T16:18:01 - 2012-05-05T17:54:06"
+    }
+}
+
+
+#
+# METHODS
+#
+def cloud_masked_rescaled_image(
+        im: ee.Image,
+        bands: list[str] = HARMONIZED_BANDS,
+        mission: Optional[int] = None) -> ee.Image:
+    """ cloud mask/rescaled landsat image
+    Args:
+        im (ee.Image): landsat image
+        bands (list[str]): optical bands to be kept
+        mission (Optional[int]):
+            one of 8, 7, 5 (see `MISSIONS` dict above)
+            if exists adds `mission` property to ee.image
+
+    Returns:
+         (ee.Image) cloud masked, scaled and offset, bands
+         maintaining im properties and timestamps.
+    """
+    im = ee.Image(im)
+    timestamp = im.date().millis()
+    qa_mask = im.select('QA_PIXEL').bitwiseAnd(0b11111).eq(0)
+    saturation_mask = im.select('QA_RADSAT').eq(0)
+    im.updateMask(qa_mask).updateMask(saturation_mask)
+    im = im.select(bands).multiply(LSAT_SCALE_FACTOR).add(LSAT_OFFSET)
+    if mission:
+        im = im.set('mission', mission)
+    return im.set('system:time_start', timestamp)
+
+
+def cloud_masked_rescaled_ic_for_mission(
+        mission: int,
+        bands: Optional[list[str]] = HARMONIZED_BANDS,
+        data_filter: Optional[ee.Filter] = None) -> ee.ImageCollection:
+    """ cloud masked rescaled landsat bands ic for mission
+    Args:
+        mission (int): one of 8, 7, 5 (see `MISSIONS` dict above)
+        data_filter (Optional[ee.Filter]): if exists, filter ic
+
+    Returns:
+         (ee.ImageCollection) of cloud masked, scaled and offset, landsat images
+    """
+    info = MISSIONS[mission]
+    ic = ee.ImageCollection(info['id'])
+    if data_filter:
+        ic = ic.filter(data_filter)
+    if bands:
+        ic = ic.select(QA_BANDS + info['bands'], QA_BANDS + bands)
+    else:
+        bands = info['bands']
+    ic = ic.map(lambda im: cloud_masked_rescaled_image(im, bands=bands, mission=mission))
+    return ic
+
+
+def harmonized_cloud_masked_rescaled_ic(
+        missions: list[int] = [5, 7, 8],
+        data_filter: Optional[ee.Filter] = None) -> ee.ImageCollection:
+    """ cloud masked rescaled landsat bands ic for mission
+    Args:
+        mission (int): one of 8, 7, 5 (see `MISSIONS` dict above)
+        data_filter (Optional[ee.Filter]): if exists, filter ic
+
+    Returns:
+         (ee.ImageCollection) of cloud masked, scaled and offset, landsat images
+    """
+    ic = cloud_masked_rescaled_ic_for_mission(missions[0], data_filter=data_filter)
+    for m in missions[1:]:
+        ic = ic.merge(cloud_masked_rescaled_ic_for_mission(m, data_filter=data_filter))
+    return ic
