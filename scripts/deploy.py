5758921dc66d5122f7736cdd0bdbda5e929b88ce
brookie guzder-williams <bguzder-williams@berkeley.edu>
2024-12-14 12:49:16 -0800

    HW test gh-pages workflow: repush

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,4 +1,9 @@
 f = open("index.html", "w")
 f.write("<h1>Hello World</h1>\n")
+f.write("<p>i will do things one <a href='/other'>day</a></p>")
+f.close()
+
+f = open("other.html", "w")
+f.write("<h1>Hello World</h1>\n")
 f.write("<p>i will do things one day</p>")
 f.close()
