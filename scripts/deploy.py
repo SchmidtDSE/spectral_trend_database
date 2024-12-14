495b39ca4ed8a745aa09dfa5703e90e10be05f96
brookie guzder-williams <bguzder-williams@berkeley.edu>
2024-12-14 13:02:16 -0800

    HW test gh-pages workflow: artifact is dir

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,18 @@
-f = open("index.html", "w")
+from pathlib import Path
+
+# Create a Path object representing the directory you want to create
+directory_path = Path("site")
+
+# Create the directory
+directory_path.mkdir(parents=True, exist_ok=True)
+
+
+f = open("site/index.html", "w")
 f.write("<h1>Hello World</h1>\n")
 f.write("<p>i will do things one <a href='/other'>day</a></p>")
 f.close()
 
-f = open("other.html", "w")
+f = open("site/other.html", "w")
 f.write("<h1>Hello World</h1>\n")
 f.write("<p>i will do things one day</p>")
 f.close()
