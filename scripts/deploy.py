48bd38ef6a8bfb6ad31ecc5120d0ecc34678c374
brookie guzder-williams <bguzder-williams@berkeley.edu>
2024-12-15 12:04:33 -0800

    missing copytree

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,17 +1,12 @@
 from pathlib import Path
 import shutil
 
-# Create a Path object representing the directory you want to create
-directory_path = Path("./_site/docs")
-
-# Create the directory
-# directory_path.mkdir(parents=True, exist_ok=True)
-
 
 print('1.')
 source_dir = Path("_build/html")
 destination_dir = Path("./_site")
 
+shutil.copytree(source_dir, destination_dir)
 
 print('1b.')
 source_dir = Path("myst/nb")
