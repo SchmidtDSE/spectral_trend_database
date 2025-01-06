34eac1cbe9d04eba44cb459015c626fd8c0c2b5c
brookie guzder-williams <bguzder-williams@berkeley.edu>
2025-01-06 12:55:00 -0800

    step-7: running

--- scripts/step-7.cover_crop_features.py
+++ scripts/step-7.cover_crop_features.py
@@ -21,50 +21,61 @@ runtime: ~ XXX minutes
 License:
     BSD, see LICENSE.md
 """
+from typing import Callable, Union, Optional, Literal, TypeAlias, Sequence, Any
+from datetime import timedelta
+from dateutil.parser import parse as dt_parse
 import pandas as pd
+import mproc
 from spectral_trend_database.config import config as c
 from spectral_trend_database import query
 from spectral_trend_database import smoothing
 from spectral_trend_database import utils
-from spectral_trend_database import runner
+from spectral_trend_database import paths
 from spectral_trend_database import types
+from spectral_trend_database import runner
 
 
 #
 # CONSTANTS
 #
-YEARS = range(2005, 2011 + 1)
+YEARS = range(2006, 2011 + 1)
 DRY_RUN = False
+IDENT_COLS = ['sample_id', 'year', 'date']
+MAP_METHOD = mproc.map_sequential
+# MAP_METHOD = mproc.map_with_threadpool
 SRC_INDICES = ['ndvi', 'evi', 'evi2']
-MACD_PARTS = [
-    'ema_a',
-    'ema_b',
-    'macd',
-    'ema_c',
-    'macd_div']
-
-
-TABLE_NAME = c.SMOOTHED_INDICES_TABLE_NAME.upper()
-# MAP_METHOD = 'sequential'
-MAP_METHOD = 'threadpool'
-REPO_PATH = '/Users/brookieguzder-williams/code/dse/COVERCROPS/spectral_trend_database/repo'
-LOCAL_DEV_PATH = f'{REPO_PATH}/nb/smoothed-sample.2020.json'
-LOCAL_DEV_DATA = False
+COLUMNS =  ['date', 'sample_id'] + SRC_INDICES
+YYYY_MM_DD_FMT = '%Y-%m-%d'
+GROWING_YEAR_BUFFER = timedelta(days=20)
 
 
 #
 # METHODS
 #
-def process_row(data: types.DICTABLE, local_dest: str, data_vars: list, list_vars: list):
-    _data = utils.row_to_xr(
-        dict(data),
-        coord=c.COORD_COLUMN,
-        data_vars=data_vars)
-    _data = smoothing.macd_processor(_data, spans=[5, 10, 5])
-    _data = utils.xr_to_row(_data)
-    _data = runner.post_process_row(_data, list_vars=list_vars)
-    # utils.append_ldjson(file_path=local_dest, data=_data)
-
+def process_rows(
+        rows: pd.DataFrame,
+        sample_id: str,
+        year: int,
+        start_date: str,
+        end_date: str,
+        local_dest: str,
+        data_vars: list):
+    try:
+        ds = rows[['date'] + data_vars].set_index('date').to_xarray()
+        ds = smoothing.macd_processor(ds, spans=[5, 10, 5])
+        ds = ds.sel(date=slice(start_date, end_date))
+        data = ds.to_dataframe().reset_index(drop=False)
+        data['date'] = data.date.apply(lambda d: d.strftime(YYYY_MM_DD_FMT))
+        data = data.to_dict('records')
+        utils.append_ldjson(
+            local_dest,
+            data,
+            sample_id=sample_id,
+            year=year,
+            multiline=True,
+            dry_run=DRY_RUN)
+    except Exception as e:
+        return dict(sample_id=sample_id, year=year, error=str(e))
 
 #
 # RUN
@@ -72,60 +83,70 @@ def process_row(data: types.DICTABLE, local_dest: str, data_vars: list, list_var
 print('\n' * 2)
 print('compute macd(-div) series:')
 print('=' * 100)
+
+from IPython.display import display
+from pprint import pprint
+
 for year in YEARS:
     print('-' * 100)
     # 1. process paths
-    table_name, dataset_name, local_dest, gcs_dest = runner.destination_strings(
-        year,
-        table_name=c.MACD_TABLE_NAME,
-        local_folder=c.MACD_FOLDER,
-        gcs_folder=c.MACD_FOLDER,
-        dataset_name=c.DATASET_NAME,
-        file_base_name=None)
+    table_name, local_dest, gcs_dest = runner.table_name_and_paths(
+        c.MACD_TABLE_NAME,
+        c.MACD_FOLDER,
+        year=year)
+    start = dt_parse(f'{year-1}-{c.OFF_SEASON_START_YYMM}')
+    end = dt_parse(f'{year}-{c.OFF_SEASON_START_YYMM}')
+    print(table_name)
+    print(local_dest)
+    print(gcs_dest)
+
 
     # 2. query data
     print('- run query:')
-    if LOCAL_DEV_DATA:
-        data = pd.read_json(LOCAL_DEV_PATH, orient='records', lines=True)
-        data = data[[c.COORD_COLUMN] + c.META_COLUMNS + SRC_INDICES]
-    else:
-        qc = query.QueryConstructor(
-            c.SMOOTHED_INDICES_TABLE_NAME,
-            table_prefix=f'{c.GCP_PROJECT}.{c.DATASET_NAME}')
-        qc.where(year=year)
-        data = query.run(
-            sql=qc.sql(),
-            to_dataframe=True,
-            # to_dataframe=False,
-            print_sql=True)
-    data = data.to_dict('records')
-    from IPython.display import display
-    from pprint import pprint
-    data_vars = runner.get_data_vars(data[0])
-    list_vars = utils.list_prefixes(SRC_INDICES, MACD_PARTS)
-    print('\t size:', len(data))
-    print('\t data_vars:', data_vars)
-    print('\t list_vars:', list_vars)
-    print()
+    qc = query.QueryConstructor(
+        c.SMOOTHED_INDICES_TABLE_NAME,
+        table_prefix=f'{c.GCP_PROJECT}.{c.DATASET_NAME}')
+    qc.select(*COLUMNS)
+    qc.where(
+        date=(start - GROWING_YEAR_BUFFER).strftime(YYYY_MM_DD_FMT),
+        date_op='>=')
+    qc.where(
+        date=(end + GROWING_YEAR_BUFFER).strftime(YYYY_MM_DD_FMT),
+        date_op='<')
+    data = query.run(
+        sql=qc.sql(),
+        to_dataframe=True,
+        print_sql=True)
+    data = data.sort_values('date')
+    sample_ids = data.sample_id.unique()
+
 
     # 3. run
-    errors = runner.mapper(MAP_METHOD)(
-        process_row,
-        data,
-        max_processes=c.MAX_PROCESSES,
-        local_dest=local_dest,
-        data_vars=data_vars,
-        list_vars=list_vars)
+    data_vars = [n for n in data.columns if n not in IDENT_COLS]
+    errors = MAP_METHOD(
+        lambda s: process_rows(
+            data[data.sample_id==s],
+            sample_id=s,
+            year=year,
+            start_date=start.strftime(YYYY_MM_DD_FMT),
+            end_date=end.strftime(YYYY_MM_DD_FMT),
+            local_dest=local_dest,
+            data_vars=data_vars),
+        sample_ids,
+        max_processes=c.MAX_PROCESSES)
+
 
     # 4. gcp
     if DRY_RUN:
-        print('- dry_run [skipping gcp]')
+        print('- dry_run [gcp]:', gcs_dest)
     else:
         runner.save_to_gcp(
-            local_dest=local_dest,
+            src=local_dest,
             gcs_dest=gcs_dest,
-            dataset_name=dataset_name,
-            table_name=table_name)
+            dataset_name=c.DATASET_NAME,
+            table_name=table_name,
+            remove_src=False)
+
 
     # 5. report on errors
     runner.print_errors(errors)
