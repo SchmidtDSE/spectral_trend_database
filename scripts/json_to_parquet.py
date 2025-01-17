75f74b9c9462d58d7541a0ec8a23848ba50460a1
brookie guzder-williams <bguzder-williams@berkeley.edu>
2025-01-17 13:46:37 -0800

    json-to-parquet: proper handling of partitioned paths

--- scripts/json_to_parquet.py
+++ scripts/json_to_parquet.py
@@ -1,3 +1,27 @@
+""" CONVERT JSON FILES TO PARQUET
+
+authors:
+    - name: Brookie Guzder-Williams
+
+affiliations:
+    - University of California Berkeley,
+      The Eric and Wendy Schmidt Center for Data Science & Environment
+
+note:
+
+    because of year partitioning you must be working with a clean local
+    directory. future versions will account for this.
+
+description:
+
+    for each (sub)folder in gcp GCS_DB_JSON convert json file to parquet and
+    save to gcp GCS_DB_PARQ
+
+runtime: ~ XXX minutes
+
+License:
+    BSD, see LICENSE.md
+"""
 import re
 from pathlib import Path
 from pprint import pprint
@@ -19,7 +43,9 @@ DRY_RUN = c.DRY_RUN
 #
 # METHODS
 #
-def process_folder(folder, process_subfolders=True):
+def process_folder(
+        folder,
+        process_subfolders=True):
     subfolders = gcp.gcs_list_folders(folder)
     if subfolders:
         if process_subfolders:
@@ -35,27 +61,23 @@ def process_folder(folder, process_subfolders=True):
             print('- (dry_run) local:', local_dest)
             print('- (dry_run) gcs:', gcs_dest)
         else:
+            Path(local_dest).parent.mkdir(parents=True, exist_ok=True)
             df = pd.concat([pd.read_json(f, lines=True) for f in files])
             print('- shape:', df.shape)
-            if True or 'year' in df.columns:
-                config = dict(partition_cols=['year'])
-            else:
-                config = {}
-            Path(local_dest).parent.mkdir(parents=True, exist_ok=True)
-            print('- local:', local_dest)
-            df.to_parquet(local_dest, **config)
-            if config:
-                print('- partitioned by year')
+            if 'year' in df.columns:
+                df.to_parquet(local_dest, partition_cols=['year'])
                 for p in Path(local_dest).glob('*/*'):
-                    print('\t- gcs:', gcs_dest)
+                    _dest = f'{gcs_dest}/{p.parent.name}/{p.name}'
+                    print('\t- gcs:', _dest)
                     runner.save_to_gcp(
                         src=p,
-                        gcs_dest=gcs_dest,
+                        gcs_dest=_dest,
                         table_name=None,
                         dataset_name=None,
                         remove_src=True,
                         dry_run=DRY_RUN)
             else:
+                df.to_parquet(local_dest)
                 print('- gcs:', gcs_dest)
                 runner.save_to_gcp(
                     src=local_dest,
